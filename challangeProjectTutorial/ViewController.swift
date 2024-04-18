71c4d4ff7ad3aa958d23ca39fb3f58d68c5468b5
OmerSarlavuk <omersarlavuk6@gmail.com>
2024-04-18 10:52:57 +0300

    Initial Commit

---
+++ challangeProjectTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challangeProjectTutorial
+//
+//  Created by Ahlatci on 18.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
